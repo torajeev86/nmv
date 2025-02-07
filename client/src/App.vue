d0408c82f089f78ee9215d696a306f4f43dbf4fb
Rajeev Jha <torajeev86@gmail.com>
2025-02-07 13:25:08 +0530

    basic crud setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,84 @@
 <template>
-  <div>
-    <h1>Welcome!</h1>
-    <p>{{ message }}</p>
+  <div id="app">
+    <h1>Vue CRUD Application</h1>
+    <div>
+      <input v-model="newItem.name" placeholder="Name" />
+      <input v-model="newItem.description" placeholder="Description" />
+      <button @click="createItem">Add Item</button>
+    </div>
+    <ul>
+      <li v-for="item in items" :key="item._id">
+        <span>{{ item.name }} - {{ item.description }}</span>
+        <button @click="deleteItem(item._id)">Delete</button>
+        <button @click="editItem(item)">Edit</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      message: "Hello from rajeev!"
+      newItem: {
+        name: '',
+        description: ''
+      },
+      items: [],
+      currentEditItem: null
     };
+  },
+  methods: {
+    async fetchItems() {
+      const res = await axios.get('http://localhost:5000/items');
+      this.items = res.data;
+    },
+    async createItem() {
+      if (this.newItem.name.trim()) {
+        await axios.post('http://localhost:5000/items', this.newItem);
+        this.newItem = { name: '', description: '' }; // Reset the form
+        this.fetchItems(); // Refresh the list
+      }
+    },
+    async deleteItem(id) {
+      await axios.delete(`http://localhost:5000/items/${id}`);
+      this.fetchItems(); // Refresh the list
+    },
+    async editItem(item) {
+      this.currentEditItem = item;
+      this.newItem.name = item.name;
+      this.newItem.description = item.description;
+    },
+    async updateItem() {
+      if (this.currentEditItem) {
+        await axios.put(`http://localhost:5000/items/${this.currentEditItem._id}`, this.newItem);
+        this.newItem = { name: '', description: '' }; // Reset the form
+        this.currentEditItem = null;
+        this.fetchItems(); // Refresh the list
+      }
+    }
+  },
+  mounted() {
+    this.fetchItems(); // Fetch items when component mounts
   }
 };
 </script>
+
+<style>
+/* Add some basic styling */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+
+input {
+  margin: 5px;
+}
+
+button {
+  margin: 5px;
+}
+</style>
